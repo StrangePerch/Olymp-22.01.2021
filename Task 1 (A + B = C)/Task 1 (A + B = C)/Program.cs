b92f51c51580a031a3001a58f28bbb5851d9d635
sasha.aav.sv <sasha.aav.sv@gmail.com>
2021-01-12 18:22:14 +0200

    Task 1

---
+++ Task 1 (A + B = C)/Task 1 (A + B = C)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1__A___B___C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Task 1 (Who have the biggest score)/Task 1 (Who have the biggest score)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1__Who_have_the_biggest_score_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scores = Console.ReadLine().Split(' ');
+
+            int p, m, o;
+            p = int.Parse(scores[0]);
+            m = int.Parse(scores[1]);
+            o = int.Parse(scores[2]);
+
+            if (p > m && p > o)
+                Console.WriteLine("Petryk");
+            if (m > p && m > o)
+                Console.WriteLine("Marichka");
+            if (o > m && o > p)
+                Console.WriteLine("Olenka");
+
+        }
+    }
+}
